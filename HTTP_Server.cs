92df9907317c129b98933100bde38c65e0ae20d2
Oscar-Tark <oscartark91@gmail.com>
2021-08-27 14:43:05 +0200

    Initial Commit

---
+++ HTTP_Server.cs
@@ -0,0 +1,20 @@
+using System.Net;
+using System.Net.Http;
+
+namespace HTTP_Server
+{
+    class HTTP_Server
+    {
+        HttpListener scorpion_http_listener;
+
+        public void startServer()
+        {
+            
+        }
+
+        public void stopServer()
+        {
+
+        }
+    }
+}
---
+++ Scorpion_HTTP.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Scorpion_HTTP_Server
+{
+    class HTTPServer
+    {
+        object version = System.Reflection.Assembly.GetExecutingAssembly();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Scorpion HTTP Server");
+        }
+    }
+}
